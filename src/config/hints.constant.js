87de73e4ad389c8baec3daddcbd7d7c989e12e13
Dvalo <guram.dvalishvili99@gmail.com>
2022-02-10 00:02:42 +0400

    Add hints config file

---
+++ src/config/hints.constant.js
@@ -0,0 +1,28 @@
+const hintsConfig = {
+  continent: {
+    type: "continent",
+    displayText: "Continent",
+    btnText: "Get Continent",
+    cost: 1,
+  },
+  bordering: {
+    type: "bordering",
+    displayText: "Bordering Countries",
+    btnText: "Get bordering countries",
+    cost: 4,
+  },
+  capital: {
+    type: "capital",
+    displayText: "Capital city",
+    btnText: "Get capital city",
+    cost: 1,
+  },
+  flag: {
+    type: "flag",
+    displayText: "",
+    btnText: "Get flag",
+    cost: 10,
+  },
+};
+
+export default hintsConfig;
